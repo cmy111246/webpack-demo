679dbd043e137d86126efb7d417943352b435d8b
mingying <mingying@deliveryherochina.com>
2015-10-30 16:04:30 +0800

    change something

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,11 @@
 
 var $ = require("jquery")
 var Modal = require("../module/tabs/tab")
-var modalCss = require("../module/tabs/tab.scss")
-var test = require("../test")
+var modalCss = require("../module/tabs/tab.scss") //编译sass
+var test = require("../test");//书写了一些能使用的es6的方法 注释的为不能用的
+
+$(".J_test").tabs();
+
+
 
 
--- static/module/tabs/tab.js
+++ static/module/tabs/tab.js
@@ -57,7 +57,7 @@ $.fn.tabs = function(option) {
       	if (!data) {
       		var data = new Tabs(this, options);
     		$this.data('dh.tabs', data);
-    		data.init();
+    		data._init();
       	}
   	})
 }
